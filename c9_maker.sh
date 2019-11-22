1a66814aefcaefa35c0da95a3484fb934b6f625e
Nicolas Julian <33948000+nicolasjulian@users.noreply.github.com>
2019-11-22 09:18:36 +0700

    Add files via upload

---
+++ c9_maker.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#Run as sudo or root user
+read -p "Input User : " user
+read -p "Input Password : " password
+read -p "Input Port (Recomend Range : 1000-5000) : " port
+
+#Create User
+sudo adduser --disabled-password --gecos "" $user
+
+#echo "$password" | passwd --stdin $user
+sudo echo -e "$password\n$password" | passwd $user
+mkdir -p /home/$user/my-projects
+
+#Get script to user directory
+git clone https://github.com/c9/core.git /home/$user/c9sdk
+sudo chown $user.$user /home/$user -R
+sudo -u $user -H sh -c "cd /home/$user/c9sdk; scripts/install-sdk.sh"
+sudo chown $user.$user /home/$user/ -R
+sudo chmod 700 /home/$user/ -R
+sudo cat > /lib/systemd/system/c9-$user.service << EOF
+
+# Run:
+# - systemctl enable c9
+# - systemctl {start,stop,restart} c9
+#
+[Unit]
+Description=c9
+After=syslog.target network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/node /home/${user}/c9sdk/server.js -a $user:$password --listen 0.0.0.0 -w /home/$user/my-projects
+Environment=NODE_ENV=production PORT=$port
+User=$user
+Group=$user
+UMask=0002
+Restart=on-failure
+
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=c9
+
+[Install]
+WantedBy=multi-user.target
+#End
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable c9-$user.service
+sudo systemctl restart c9-$user.service
+sleep 10
+sudo systemctl status c9-$user.service
